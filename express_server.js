eba17de0869e26e0e552af751b68a9dc5a64ec0c
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-29 16:35:12 -0700

    Add a POST route for /register

--- express_server.js
+++ express_server.js
@@ -13,6 +13,20 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -26,7 +40,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]] };
   res.render("urls_index", templateVars);
 });
 
@@ -35,7 +49,7 @@ const generateRandomString = () => {
 };
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: users[req.cookies["user_id"]] };
 
   res.render("urls_new", templateVars);
 });
@@ -44,8 +58,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"],
-  };
+    user: users[req.cookies["user_id"]],  };
   res.render("urls_show", templateVars);
 });
 
@@ -73,23 +86,34 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
-// handle login
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  res.cookie("username", username);
-  res.redirect("/urls");
-});
-
-// handle logout
-app.get("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
-});
 
 // handle register
 app.get("/register", (req, res) => {
   res.render("register");
 });
+
+// handle register post
+app.post("/register", (req, res) => {
+  const id = generateRandomString();
+  const email = req.body.email;
+  const password = req.body.password;
+  const  user = {
+    id,
+    email,
+    password,
+  };
+  users[id] = user;
+  res.cookie("user_id", id);
+  res.redirect("/urls");
+});
+
+// handle logout
+app.get("/logout", (req, res) => {
+  res.clearCookie("user_id");
+  res.redirect("/urls");
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
